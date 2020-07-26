b09ef03c07bbef47ddd79ea22c82f83279df79cf
Esteves <esteves_m_lima@hotmail.com>
2020-07-26 17:07:30 -0300

    fix(minor fix middlewares wrapping)

--- base-service/src/api/middlewares/adjust-request.js
+++ base-service/src/api/middlewares/adjust-request.js
@@ -1,6 +1,9 @@
-const asyncHandler = require('../../helpers/async/async-handler');
+const wrapAsync = require('../../helpers/async/wrap-async');
 const ErrorResponse = require('../../helpers/error/structure/error-response');
 
-exports.adjustFunction = asyncHandler(async (req, res, next) => {
+exports.adjustFunction = (req, res, next) => {
   next();
-});
+};
+
+// Wrapping all functions for error catching
+wrapAsync.wrapAsyncFunctions(this);
