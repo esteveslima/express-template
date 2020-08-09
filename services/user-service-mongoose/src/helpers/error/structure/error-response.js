0e23ba5aee4106538aba1c5fa47bbaea9d876602
Esteves <esteves_m_lima@hotmail.com>
2020-08-08 21:43:21 -0300

    fix(minor refactor error)

--- services/user-service-mongoose/src/helpers/error/structure/error-response.js
+++ services/user-service-mongoose/src/helpers/error/structure/error-response.js
@@ -19,28 +19,35 @@ ErrorResponse.parse = (err) => {
   // ...tests the error and parse to a proper ErrorResponse object
   // May replace generic responses words to cover all fields
   let errorCode = { ...errorCodes.INTERNAL_SERVER_ERROR };
-  const errorResult = `${err}`;
+  let errorResult = `${err}`;
 
-  if (err.name === 'MongoError') {
-    switch (err.code) {
-      case 11000: {
-        errorCode = { ...errorCodes.DUPLICATED_KEY };
-        const field = Object.keys(err.keyValue)[0];
-        errorCode.message = errorCode.message.replace(/\b(field)\b/gi, field);
-        break;
+  switch (true) {
+    case (err.name === 'MongoError'): {
+      switch (err.code) {
+        case 11000: {
+          errorCode = { ...errorCodes.DUPLICATED_KEY };
+          const field = Object.keys(err.keyValue)[0];
+          errorCode.message = errorCode.message.replace(/\b(field)\b/gi, field);
+          break;
+        }
+        default: {
+          break;
+        }
       }
-      default: {
-        break;
-      }
+      break;
     }
-  } else if (err.name === 'ValidationError') {
-    errorCode = { ...errorCodes.INVALID_FIELD };
-    const validationMessage = err.errors[Object.keys(err.errors)[0]].properties ? err.errors[Object.keys(err.errors)[0]].properties.message : '';
-    const field = err.errors[Object.keys(err.errors)[0]].path;
-    const input = err.errors[Object.keys(err.errors)[0]].value;
-    errorCode.message = `${errorCode.message.replace(/\b(field)\b/gi, field).replace(/\b(value)\b/gi, input)} ${validationMessage}`;
-  } else if (err) {
-
+    case (err.name === 'ValidationError'): {
+      errorCode = { ...errorCodes.INVALID_FIELD };
+      const validationMessage = err.errors[Object.keys(err.errors)[0]].properties ? err.errors[Object.keys(err.errors)[0]].properties.message : '';
+      const field = err.errors[Object.keys(err.errors)[0]].path;
+      const input = err.errors[Object.keys(err.errors)[0]].value;
+      errorCode.message = `${errorCode.message.replace(/\b(field)\b/gi, field).replace(/\b(value)\b/gi, input)} ${validationMessage}`;
+      break;
+    }
+    default: {
+      errorCode = { ...errorCodes.INTERNAL_SERVER_ERROR };
+      errorResult = `${err}`;
+    }
   }
 
   return new ErrorResponse(errorCode, errorResult);
