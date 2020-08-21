5a21b6eb8132d808908465b6f944fe53feefb96a
Esteves <esteves_m_lima@hotmail.com>
2020-08-21 14:50:53 -0300

    feat(k8s config, docker-compose nginx config, minor fixes)

--- services/user-service-mongoose/src/loaders/mongoose/mongoose.js
+++ services/user-service-mongoose/src/loaders/mongoose/mongoose.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
 
 exports.connect = async () => {
-  const connection = await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  });
-
-  console.log(`MongoDB connected(${connection.connection.host})`);
+  try {
+    const connection = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB connected(${connection.connection.host})`);
+  } catch (e) {
+    console.log(`Failed to connect to database ${process.env.MONGO_URI}`);
+    process.exit(0);
+  }
 };
